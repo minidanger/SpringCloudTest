8f3a5545d86d50d1f67faa25f104fd24da143f57
minidanger <15021771582@163.com>
2022-01-28 14:00:29 +0800

    backup

--- clound-provider-payment8001/src/main/java/com/lzr/springcloudtest/controller/PaymentController.java
+++ clound-provider-payment8001/src/main/java/com/lzr/springcloudtest/controller/PaymentController.java
@@ -4,6 +4,8 @@ import com.lzr.springcloudtest.entities.CommonResult;
 import com.lzr.springcloudtest.entities.Payment;
 import com.lzr.springcloudtest.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,7 @@ public class PaymentController
     public CommonResult creat(Payment payment)
     {
         int result = paymentService.creat(payment);
-        log.info("******插入结果："+result);
+        //log.info("******插入结果："+result);
         if(result>0)
         {
             return new CommonResult(200,"插入成功");
@@ -30,4 +32,13 @@ public class PaymentController
             return new CommonResult(444,"插入成功");
         }
     }
+    @GetMapping(value = "/payment/get/{id}")
+    public  int  getpay(@PathVariable("id") Long id)
+    {
+        Payment payment = paymentService.getPaymentByid(id);
+        System.out.println(payment);
+        return  1;//new CommonResult(444,"插入成功",payment);
+    }
+
+
 }
--- clound-provider-payment8001/src/main/java/com/lzr/springcloudtest/service/impl/PaymentServiceImpl.java
+++ clound-provider-payment8001/src/main/java/com/lzr/springcloudtest/service/impl/PaymentServiceImpl.java
@@ -2,13 +2,14 @@ package com.lzr.springcloudtest.service.impl;
 
 import com.lzr.springcloudtest.dao.PaymentDao;
 import com.lzr.springcloudtest.entities.Payment;
+import com.lzr.springcloudtest.service.PaymentService;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
-public class PaymentServiceImpl
+public class PaymentServiceImpl implements PaymentService
 {
     @Resource
     private PaymentDao paymentDao;
@@ -18,6 +19,7 @@ public class PaymentServiceImpl
         return paymentDao.creat(payment);
     }
     public Payment getPaymentByid(@Param("id") Long id){
+
         return paymentDao.getPaymentByid(id);
     }
 }
